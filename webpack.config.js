d18b7c9439158e82cacce9df38fbf524884c9f02
dato7898 <davidd7598@gmail.com>
2022-02-12 14:54:47 +0600

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: path.join(__dirname, 'src', 'main', 'resources', 'static', 'js', 'main.js'),
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        },
+        compress: true,
+        port: 8000,
+        allowedHosts: [
+            'localhost:9000'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src', 'main', 'resources', 'static', 'js'),
+            path.join(__dirname, 'node_modules')
+        ]
+    }
+}
